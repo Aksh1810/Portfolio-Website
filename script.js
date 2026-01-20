5b28b84184a76ec3959190d295648d4d89a4548a
Aksh1810 <akshp427@gmail.com>
2026-01-20 16:40:43 -0600

    Cursor length fixed

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // --- Custom Cursor Implementation ---
     const cursorContainer = document.getElementById('cursor-container');
-    const trailLength = 20;
+    const trailLength = 12;
     const trails = [];
 
     // Create trail elements
